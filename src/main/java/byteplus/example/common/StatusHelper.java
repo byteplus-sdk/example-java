bf4035e35275fbab16d6c8e0ff7ba72d81d34076
zhangyu <zhangyu.lzj@bytedance.com>
2022-11-04 18:49:25 +0800

    fix(*): fix status bug

--- src/main/java/byteplus/example/common/StatusHelper.java
+++ src/main/java/byteplus/example/common/StatusHelper.java
@@ -9,19 +9,21 @@ import static byteplus.sdk.core.Constant.STATUS_CODE_SUCCESS;
 import static byteplus.sdk.core.Constant.STATUS_CODE_TOO_MANY_REQUEST;
 
 public class StatusHelper {
+    public static final Integer HTTP_STATUS_OK = 200;
+
     public static boolean isUploadSuccess(Status status) {
         int code = status.getCode();
         // It is still considered as success, which is rejected for idempotent
-        return code == STATUS_CODE_SUCCESS || code == STATUS_CODE_IDEMPOTENT;
+        return code == STATUS_CODE_SUCCESS || code == STATUS_CODE_IDEMPOTENT || code == HTTP_STATUS_OK;
     }
 
     public static boolean isSuccess(Status status) {
         int code = status.getCode();
-        return code == STATUS_CODE_SUCCESS;
+        return code == STATUS_CODE_SUCCESS || code == HTTP_STATUS_OK;
     }
 
     public static boolean isSuccess(int code) {
-        return code == STATUS_CODE_SUCCESS;
+        return code == STATUS_CODE_SUCCESS|| code == HTTP_STATUS_OK;
     }
 
     public static boolean isServerOverload(Status status) {
